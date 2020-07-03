96a2c5a2cc9a85fbedcdd8c17ed0b078a16da070
dajo-ariando <dajo.ariando@gmail.com>
2020-07-03 02:53:54 -0400

    working version before IEEE APS/URSI 2020

--- 200701 Tcl script for Quartus (working).tcl
+++ 200701 Tcl script for Quartus (working).tcl
@@ -11,7 +11,10 @@ open_service master $master
 set led_addr 0x20
 set sw_addr 0x10
 set dly_addr 0x30
-set bitstream 0x40
+set datain3 0x50
+set datain2 0x60
+set datain1 0x70
+set datain0 0x40
 set start	0x00
 
 # Write to the LEDs.
@@ -22,13 +25,39 @@ set start	0x00
 # Do this several times and change the switches state
 # master_read_16 $master $sw_addr 1
 
-# Write delay
-master_write_32 $master $dly_addr 0x07
+# Write delay. Avoid to use 0x01 and (CLK_DIV1/2-1) for the delay number because it will reduce the clock a little bit. All numbers between are valid.
+master_write_32 $master $dly_addr 0x4
 
 # Send bit sequence
-# master_write_32 $master $bitstream 0xFFFFFFFF
-# master_write_32 $master $bitstream 0x00000000
-master_write_32 $master $bitstream 0xFF0000F0
+# set x [expr {round(rand()*0xFFFFFFFF)}]
+# master_write_32 $master $datain3 $x
+# set x [expr {round(rand()*0xFFFFFFFF)}]
+# master_write_32 $master $datain2 $x
+# set x [expr {round(rand()*0xFFFFFFFF)}]
+# master_write_32 $master $datain1 $x
+# set x [expr {round(rand()*0xFFFFFFFF)}]
+# master_write_32 $master $datain0 $x
+
+
+master_write_32 $master $datain3 0x0F020FDF
+master_write_32 $master $datain2 0x54A3A0DF
+master_write_32 $master $datain1 0x1B6F0B70
+master_write_32 $master $datain0 0xFCC8090E
+
+# master_write_32 $master $datain3 0xF0F0F0F0
+# master_write_32 $master $datain2 0xFF00FF00
+# master_write_32 $master $datain1 0xFFFF0000
+# master_write_32 $master $datain0 0xFFFF0000
+
+# master_write_32 $master $datain3 0xFFFFFFFF
+# master_write_32 $master $datain2 0xFFFFFFFF
+# master_write_32 $master $datain1 0xFFFFFFFF
+# master_write_32 $master $datain0 0xFFFFFFFF
+
+# master_write_32 $master $datain3 0x0
+# master_write_32 $master $datain2 0x0
+# master_write_32 $master $datain1 0x0
+# master_write_32 $master $datain0 0x0
 
 # Start the sequence
 master_write_16 $master $start 0x1
